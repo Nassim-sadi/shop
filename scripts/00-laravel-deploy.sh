2d9645ec1b4015b01ae2afce2a036e34d8cd749a
nassim.sadi <nassim.sadi@hotmail.com>
2024-10-07 13:38:58 +0100

    fix : added npm download to script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,13 @@ echo "Running composer"
 cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
+# Install Node.js and npm if not detected automatically
+if ! command -v npm &> /dev/null
+then
+    echo "npm not found, installing Node.js"
+    curl -sL https://deb.nodesource.com/setup_lts.x | bash -
+    apt-get install -y nodejs
+fi
 echo "npm build"
 npm install
 npm run build
