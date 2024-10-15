029252c6afd9688b19c3c47471c7e16cb904e46d
nassim.sadi <nassim.sadi@hotmail.com>
2024-10-15 14:22:07 +0100

    fix : fixed not found

--- resources/js/router/Index.js
+++ resources/js/router/Index.js
@@ -45,11 +45,10 @@ const router = createRouter({
             redirect: "/admin",
         },
         {
-            path: "/pages/notfound",
+            path: "/admin/:catchAll(.*)",
             name: "notfound",
             component: () => import("@/views/admin/NotFound.vue"),
         },
-
         {
             path: "/auth/login",
             name: "login",
@@ -86,26 +85,20 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     const auth = authStore();
     ability.update(auth.permissions);
-    if (
-        to.name !== "login" &&
-        to.name !== "forgot-password" &&
-        to.name !== "reset-password"
-    ) {
-        if (isUserLoggedIn(auth)) {
-            if (canNavigate(to.name)) {
-                next();
-            } else {
-                next({ name: "accessDenied" });
-            }
-        } else {
-            next({ name: "login" });
-        }
-    } else {
-        if (isUserLoggedIn(auth)) {
-            next("/");
-        } else {
-            next();
-        }
+    if (to.meta.requiresAuth && !isUserLoggedIn(auth)) {
+        return next({ name: "login" });
     }
+    if (to.meta.isAdmin && !canNavigate(to.name)) {
+        return next({ name: "accessDenied" });
+    }
+    const publicPages = ["login", "forgot-password", "reset-password"];
+    if (publicPages.includes(to.name)) {
+        if (isUserLoggedIn(auth)) {
+            return next({ name: "dashboard" });
+        }
+        return next();
+    }
+    next();
 });
+
 export default router;
