286ecd912af66fbf7298ef51dfdb1d78273128b4
nassim.sadi <nassim.sadi@hotmail.com>
2024-11-06 15:39:25 +0100

    fix : fixed translation for nav menu

--- resources/js/constants/navigation/Index.js
+++ resources/js/constants/navigation/Index.js
@@ -1,9 +1,9 @@
 export default [
     {
-        label: "Home",
+        label: "navigation.home",
         items: [
             {
-                label: "Dashboard",
+                label: "navigation.dashboard",
                 icon: "pi pi-fw pi-home",
                 to: "/",
                 color: "#238E3A",
@@ -12,32 +12,32 @@ export default [
     },
 
     {
-        label: "Pages",
+        label: "navigation.pages",
         icon: "ti ti-briefcase",
         items: [
             {
-                label: "Users",
+                label: "navigation.users",
                 icon: "ti ti-users",
                 to: "/admin/users",
                 color: "#3366bb",
                 access: "user_access",
             },
             {
-                label: "Activity History",
+                label: "navigation.activity_history",
                 icon: "ti ti-history",
                 to: "/admin/activity-history",
                 color: "#830123",
                 access: "activities_access",
             },
             {
-                label: "Roles",
+                label: "navigation.roles",
                 icon: "ti ti-lock",
                 to: "/admin/roles",
                 color: "#238E3A",
                 access: "role_access",
             },
             {
-                label: "Settings",
+                label: "navigation.settings",
                 icon: "ti ti-settings",
                 to: "/admin/settings",
                 color: "#9C27B0",
